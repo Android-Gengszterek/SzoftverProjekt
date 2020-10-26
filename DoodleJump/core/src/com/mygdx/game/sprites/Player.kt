a4d9fe2f463effdeb747e041c66ad992290d8870
gzsolt11 <gergelyzsolt2001@gmail.com>
2020-10-26 12:43:16 +0200

    Added green platforms in the game and created endless map, update for platforms and background

--- DoodleJump/core/src/com/mygdx/game/sprites/Player.kt
+++ DoodleJump/core/src/com/mygdx/game/sprites/Player.kt
@@ -1,5 +1,6 @@
 package com.mygdx.game.sprites
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector3
@@ -12,12 +13,14 @@ class Player{
     var velocity: Vector3
     var playerTexture: Texture
     var bounds: Rectangle
+    var isFalling: Boolean
 
     constructor(x:Int, y:Int){
         position = Vector3(x.toFloat(),y.toFloat(),0f)
         velocity = Vector3(0f,0f,0f)
         playerTexture = Texture("player.png")
         bounds = Rectangle(position.x, position.y, playerTexture.width.toFloat(), playerTexture.height.toFloat())
+        isFalling = true
 
     }
 
@@ -25,6 +28,16 @@ class Player{
         velocity.add(0f,GRAVITY,0f)
         velocity.scl(dt)
         position.add(velocity.x,velocity.y,0f)
+        if(position.x+100f < 0f){
+            position.x = Gdx.graphics.width.toFloat()
+        }
+
+        if(position.x > Gdx.graphics.width.toFloat()){
+            position.x = 0f
+        }
+        if(velocity.y < 0){
+            isFalling = true
+        }
 
         velocity.scl(1/dt)
         bounds.setPosition(position.x,position.y)
@@ -41,10 +54,15 @@ class Player{
 
     fun jump():Unit{
         velocity.y = 1200f
+        isFalling = false
     }
 
     fun stop():Unit{
         velocity.x = 0f
+    }
+
+    fun stopFall():Unit{
+        velocity.y = 0f
     }
 
 
--- DoodleJump/core/src/com/mygdx/game/states/PlayState.kt
+++ DoodleJump/core/src/com/mygdx/game/states/PlayState.kt
@@ -8,30 +8,64 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
 import com.mygdx.game.MyGdxGame
 import com.mygdx.game.sprites.Floor
+import com.mygdx.game.sprites.GreenPlatform
 import com.mygdx.game.sprites.Player
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class PlayState(gameStateManager: GameStateManager): State(gameStateManager){
+    companion object{
+        private var PLATFORM_SPACING:Float = 500f
+        private var PLATFORM_COUNT:Int = 4
+        private var PLATFORM_HEIGHT:Float = 60f
+    }
     private lateinit var backgroundImage: Texture
+    private lateinit var backgroundImage2: Texture
     private lateinit var floor: Floor
     private lateinit var player: Player
+    private lateinit var rand: Random
+    private var platforms: java.util.ArrayList<GreenPlatform>
+    lateinit var backgroundImagePosition: Vector2
+    lateinit var backgroundImage2Position: Vector2
 
     init {
         backgroundImage = Texture("background.png")
+        backgroundImagePosition = Vector2(cam.position.x-cam.viewportWidth/2,0f)
+        backgroundImage2 = Texture("background.png")
+        backgroundImage2Position = Vector2(cam.position.x-cam.viewportWidth/2,Gdx.graphics.height.toFloat())
+
         floor = Floor()
-        player = Player(500,1000)
+        player = Player(500,100)
+        platforms = ArrayList<GreenPlatform>()
+
+        for(i in 0..PLATFORM_COUNT){
+            rand = Random()
+            if(i * PLATFORM_SPACING + PLATFORM_HEIGHT > floor.floorPosition.y + floor.floorTexture.height) {
+                platforms.add(GreenPlatform(i * (PLATFORM_SPACING + PLATFORM_HEIGHT)))
+            }
+           /* if(i == rand.nextInt(PLATFORM_COUNT)){
+                platforms.add(GreenPlatform(i * PLATFORM_SPACING + 60f))
+            }*/
+        }
         cam.setToOrtho(false,Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
 
     }
 
     override fun handleInput() {
         if(Gdx.input.justTouched()) {
+            if(player.position.y < 400f){
+                player.jump()
+            }
             if (Gdx.input.x < Gdx.graphics.width / 2) {
                 Gdx.app.log("Debug", "Touched!");
                 player.goLeft()
+                //player.jump()
             }
             if (Gdx.input.x > Gdx.graphics.width / 2) {
                 Gdx.app.log("Debug", "Touched!");
                 player.goRight()
+                //player.jump()
             }
         }
         if(!Gdx.input.isTouched()){
@@ -43,11 +77,28 @@ class PlayState(gameStateManager: GameStateManager): State(gameStateManager){
 
     override fun update(dt: Float) {
         handleInput()
+        updateBackground()
         player.update(dt)
+        if(player.position.y > Gdx.graphics.height/2) {
+            cam.position.y = player.position.y
+        }
 
         if(floor.collide(player.bounds)){
-            player.jump()
+            //player.jump()
+            player.stopFall()
         }
+
+
+        for(platform in platforms){
+            if((player.isFalling) && platform.collide(player.bounds)){
+                player.jump()
+            }
+            if(player.position.y-(cam.viewportHeight/2)  > platform.platformPosition.y + PLATFORM_HEIGHT){
+                platform.reposition(platform.platformPosition.y + (PLATFORM_COUNT * (PLATFORM_SPACING + PLATFORM_HEIGHT)))
+            }
+        }
+        cam.update()
+
     }
 
 
@@ -55,14 +106,29 @@ class PlayState(gameStateManager: GameStateManager): State(gameStateManager){
     override fun render(spriteBatch: SpriteBatch) {
         spriteBatch.projectionMatrix = cam.combined
         spriteBatch.begin()
-        spriteBatch.draw(backgroundImage,0f,0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
-        spriteBatch.draw(floor.floorTexture,floor.floorPosition.x,floor.floorPosition.y,Gdx.graphics.width.toFloat()*2,500f)
+        spriteBatch.draw(backgroundImage,backgroundImagePosition.x,backgroundImagePosition.y, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        spriteBatch.draw(backgroundImage2,backgroundImage2Position.x,backgroundImage2Position.y, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        spriteBatch.draw(floor.floorTexture,0f-(Gdx.graphics.width/2),floor.floorPosition.y,Gdx.graphics.width.toFloat()*2,500f)
         spriteBatch.draw(player.playerTexture, player.position.x, player.position.y,300f,300f)
+
+        //spriteBatch.draw(platforms[0].greenPlatformTexture, platforms[0].platformPosition.x,platforms[0].platformPosition.y,200f,60f)
+        for(platform in platforms){
+            spriteBatch.draw(platform.greenPlatformTexture, platform.platformPosition.x,platform.platformPosition.y,200f,60f)
+        }
         spriteBatch.end()
     }
 
     override fun dispose() {
         backgroundImage.dispose()
+    }
+
+    fun updateBackground():Unit{
+        if(player.position.y-(cam.viewportHeight)   > backgroundImagePosition.y + (Gdx.graphics.height.toFloat()/2)){
+            backgroundImagePosition.add(0f,Gdx.graphics.height.toFloat()*2)
+        }
+        if(player.position.y-(cam.viewportHeight)    > backgroundImage2Position.y+ (Gdx.graphics.height.toFloat()/2)){
+            backgroundImage2Position.add(0f,Gdx.graphics.height.toFloat()*2)
+        }
     }
 
 
