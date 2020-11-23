2e25dec23cadf49708e29a58d397f9ef530ad1ea
Botond Halasz <halasz_boti_99@yahoo.com>
2020-11-23 19:50:27 +0200

    xml created

---
+++ DoodleJump/android/src/com/mygdx/game/ui/activitys/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.mygdx.game.ui.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mygdx.game.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
