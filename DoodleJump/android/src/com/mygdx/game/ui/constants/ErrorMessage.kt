e6b3b1ac759f8525e55cb8d12d903a0196e4faae
Botond Halasz <halasz_boti_99@yahoo.com>
2020-12-05 15:55:21 +0200

    Wrong error message

--- DoodleJump/android/src/com/mygdx/game/ui/constants/ErrorMessage.kt
+++ DoodleJump/android/src/com/mygdx/game/ui/constants/ErrorMessage.kt
@@ -2,7 +2,7 @@ package com.mygdx.game.ui.constants
 
 object ErrorMessage {
     val EMPTY_FIELD = "Fill out please!"
-    val USERNAME_NOT_EXIST = "Try again later or check your internet connection!"
+    val USERNAME_NOT_EXIST = "This Username not exist!"
     val PASSWORD = "Wrong password!"
     val USERNAME_LENGTH = "Minimum 5, maximum 10 character"
     val USERNAME_EXIST = "This username is already exist"
